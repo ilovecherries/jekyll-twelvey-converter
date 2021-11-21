9afa05ff4f11f6bdb86c9a3d0b7b1981a4d52c08
Cherry <ilovecherries@protonmail.com>
2021-11-21 18:14:28 -0500

    wrong class once again holy hell

--- lib/jekyll/converters/twelvey.rb
+++ lib/jekyll/converters/twelvey.rb
@@ -1,18 +1,20 @@
 module Jekyll
-    class TwelveY < Converter
-        safe false
-        priority :low
+    module Converters
+        class TwelveY < Converter
+            safe false
+            priority :low
 
-        def matches(ext)
-            ext =~ /^\.12$/i
+            def matches(ext)
+                ext =~ /^\.12$/i
+            end
+
+            def output_ext(ext)
+                ".html"
+            end
+
+            def convert(content)
+                output = `node _12y.js <<EOF\n#{content}\nEOF`
+            end   
         end
-
-        def output_ext(ext)
-            ".html"
-        end
-
-        def convert(content)
-            output = `node _12y.js <<EOF\n#{content}\nEOF`
-        end   
     end
 end
